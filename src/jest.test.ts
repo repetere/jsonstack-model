94e4e5969ca473748047ebd4742beaf35dea5862
yaw joseph etse <yaw.etse@gmail.com>
2020-04-16 18:33:31 -0400

    fixing empty test

--- src/jest.test.ts
+++ src/jest.test.ts
@@ -14,3 +14,18 @@ export function toBeWithinRange(received, floor, ceiling) {
     };
   }
 };
+
+expect.extend({
+  toBeWithinRange,
+});
+
+describe('toBeWithinRage', () => {
+  it('numeric ranges', () => {
+    expect(100).toBeWithinRange(90, 110);
+    expect(101).not.toBeWithinRange(0, 100);
+    expect({ apples: 6, bananas: 3 }).toEqual({
+      apples: expect.toBeWithinRange(1, 10),
+      bananas: expect.not.toBeWithinRange(11, 20),
+    });
+  });
+});
