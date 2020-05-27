de473fbf14f67b1b5e5447a73c06508bff3f080c
yaw joseph etse <yaw.etse@gmail.com>
2020-05-27 19:19:00 -0400

    removed rollup

--- build/test/mock/data/stopwords.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const stop_words: string[];
-export declare const norm_bible: string[];
-export declare const norm_bible_matrix: string[][];
-export declare const products: string[][];
-export declare const furniture: string[][];
