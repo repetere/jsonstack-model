962f96bc62eb52b7ab3c3e77782e540c7d2303b8
Yaw Joseph Etse <yaw.etse@gmail.com>
2022-05-22 11:38:52 -0400

    fix: type error on scikit models

--- src/ml_regression.ts
+++ src/ml_regression.ts
@@ -19,6 +19,7 @@ export class MachineLearningLinearRegression extends MachineLearningModelInterfa
       compile: {
       },
       fit: {
+        callbacks:[]
       },
       ...options,
     };
